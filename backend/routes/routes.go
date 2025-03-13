814c791b9b0d85088ed4d1079221e75aa90542f4
15 <2943603313@qq.com>
2025-03-13 21:47:52 +0800

    侧边栏新对话后端初步

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -11,6 +11,8 @@ func SetupRoutes(r *gin.Engine) {
 	r.POST("/login", services.UserLogin)
 	r.POST("/register", services.UserRegister)
 	r.POST("/send/email/code", services.EmailHandler)
+	r.POST("/chat/new",services.NewChat)
+	r.POST("/chat/:id",services.GetChat)
 	api := r.Group("/api")
 	api.Use(middleware.AuthSession)
 	api.POST("/change/username", services.ChangeUserName)
