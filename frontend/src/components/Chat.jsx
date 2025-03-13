b7016d676155be457047c94be10e03dd48a1038b
15 <2943603313@qq.com>
2025-03-13 20:17:39 +0800

    限制主页滑动

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -18,7 +18,7 @@ function ChatPage() {
   const [isOpen, setIsOpen] = useState(true); // 控制侧边栏展开/折叠
 
   return (
-    <div className="flex flex-row  min-h-screen bg-gray-100 gap">
+    <div className="flex flex-row  max-h-screen bg-gray-100 gap">
       {/*侧边栏 */}
       <div
         className={`${
@@ -26,8 +26,8 @@ function ChatPage() {
             ? " translate-x-0 md:relative md:z-auto"
             : "-translate-x-full hidden"
         } 
-             md:flex  md:w-1/5 absolute w-3/5 z-50 inset-y-0 left-0 h-screen flex flex-col justify-between border-e 
-              border-gray-100 bg-white shadow-sm transition-all duration-300 overflow-hidden`}
+             md:flex  md:w-1/5 absolute w-3/5 z-50 inset-y-0 left-0 flex flex-col max-h-screen justify-between border-e 
+              border-gray-100 bg-white shadow-sm transition-all duration-300 overflow-hidden overflow-y-auto`}
       >
         <div className="px-4 py-3 transform">
           {/*侧边栏顶部*/}
@@ -65,7 +65,7 @@ function ChatPage() {
           </div>
 
           {/*历史对话*/}
-          <ul className="mt-6 space-y-1">
+          <ul className="mt-6 space-y-60">
             <li>
               <a
                 href="#"
@@ -73,122 +73,49 @@ function ChatPage() {
               >
                 对话
               </a>
-            </li>
+                      </li>
+                      <li>
+                          <a
+                              href="#"
+                              className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+                          >
+                              对话
+                          </a>
+                      </li>
+                      <li>
+                          <a
+                              href="#"
+                              className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+                          >
+                              对话
+                          </a>
+                      </li>
+                      <li>
+                          <a
+                              href="#"
+                              className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+                          >
+                              对话
+                          </a>
+                      </li>
+                      <li>
+                          <a
+                              href="#"
+                              className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+                          >
+                              对话
+                          </a>
+                      </li>
+                      <li>
+                          <a
+                              href="#"
+                              className="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+                          >
+                              对话
+                          </a>
+                      </li>
 
-            <li>
-              <details className="group [&_summary::-webkit-details-marker]:hidden">
-                <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-                  <span className="text-sm font-medium"> Teams </span>
-
-                  <span className="shrink-0 transition duration-300 group-open:-rotate-180">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="size-5"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                </summary>
-
-                <ul className="mt-2 space-y-1 px-4">
-                  <li>
-                    <a
-                      href="#"
-                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                    >
-                      Banned Users
-                    </a>
-                  </li>
-
-                  <li>
-                    <a
-                      href="#"
-                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                    >
-                      Calendar
-                    </a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-
-            <li>
-              <a
-                href="#"
-                className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-              >
-                Billing
-              </a>
-            </li>
-
-            <li>
-              <a
-                href="#"
-                className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-              >
-                Invoices
-              </a>
-            </li>
-
-            <li>
-              <details className="group [&_summary::-webkit-details-marker]:hidden">
-                <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-                  <span className="text-sm font-medium"> Account </span>
-
-                  <span className="shrink-0 transition duration-300 group-open:-rotate-180">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="size-5"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                </summary>
-
-                <ul className="mt-2 space-y-1 px-4">
-                  <li>
-                    <a
-                      href="#"
-                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                    >
-                      Details
-                    </a>
-                  </li>
-
-                  <li>
-                    <a
-                      href="#"
-                      className="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                    >
-                      Security
-                    </a>
-                  </li>
-
-                  <li>
-                    <form action="#">
-                      <button
-                        type="submit"
-                        className="w-full rounded-lg px-4 py-2 [text-align:_inherit] text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-                      >
-                        Logout
-                      </button>
-                    </form>
-                  </li>
-                </ul>
-              </details>
-            </li>
+            
           </ul>
         </div>
       </div>
@@ -197,7 +124,7 @@ function ChatPage() {
       <div
         className={`${
           isOpen ? "w-4/5" : "w-full"
-        } flex flex-col h-full bg-gray-100`}
+        } flex flex-col h-full max-h-screen bg-gray-100`}
       >
         {/* 头部导航栏 - 固定在主内容顶部 */}
         {/*进入具体内容页可加上: border-b border-gray-300 */}
