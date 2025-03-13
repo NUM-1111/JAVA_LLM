79f7e049dc1491d6551aee19b8f4a54b054b3694
CXRunfree <2654559534@qq.com>
2025-03-13 19:04:59 +0800

    完成输入框

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,6 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { SiderBarIcon,AddIcon,NewChatIcon,BreadcrumbIcon} from "./svg-icons";
+import {
+  SiderBarIcon,
+  AddIcon,
+  NewChatIcon,
+  BreadcrumbIcon,
+  DeepThinkIcon,
+  ArrowUpIcon,
+} from "./svg-icons";
 
 function ChatPage() {
   const navigate = useNavigate(); // 获取导航函数
@@ -22,42 +29,40 @@ function ChatPage() {
              md:flex  md:w-1/5 absolute w-3/5 z-50 inset-y-0 left-0 h-screen flex flex-col justify-between border-e 
               border-gray-100 bg-white shadow-sm transition-all duration-300 overflow-hidden`}
       >
-              <div className="px-4 py-3 transform">
-                  {/*侧边栏顶部*/}
-                  <header className="top-0 flex flex-row transform">
-                      {/* 关闭按钮 */}
-                      <button
-                          onClick={() => setIsOpen(false)}
-                          className={`${isOpen ? "block" : "hidden "
-                              }  flex justify-center items-center size-10  rounded-lg hover:shadow-md hover:bg-blue-300`}
-                      >
-                          <SiderBarIcon />
-                      </button>
-                      {/*新对话按钮(icon) */}
-                      <button
-                          className={`${isOpen ? "block" : "hidden "
-                              } flex justify-center items-center size-10  rounded-lg hover:shadow-md hover:bg-blue-300 `}
-                      >
-                          <NewChatIcon />
-                      </button>
-                  </header>
+        <div className="px-4 py-3 transform">
+          {/*侧边栏顶部*/}
+          <header className="top-0 flex flex-row transform">
+            {/* 关闭按钮 */}
+            <button
+              onClick={() => setIsOpen(false)}
+              className={`${
+                isOpen ? "block" : "hidden "
+              }  flex justify-center items-center size-10  rounded-lg hover:shadow-md hover:bg-blue-300`}
+            >
+              <SiderBarIcon />
+            </button>
+            {/*新对话按钮(icon) */}
+            <button
+              className={`${
+                isOpen ? "block" : "hidden "
+              } flex justify-center items-center size-10  rounded-lg hover:shadow-md hover:bg-blue-300 `}
+            >
+              <NewChatIcon />
+            </button>
+          </header>
 
           {/*新对话模块*/}
-                  <div className="flex flex-row mt-8 font-bold text-sm rounded-lg border border-blue-200 text-blue-600 bg-blue-500/15  hover:bg-blue-500/20 ">
-                      {/*AddIcon图标*/}
-                      <div
-                          className="flex px-2 py-2.5">
-                          <AddIcon />
-                      </div>
+          <div className="flex flex-row mt-8 font-bold text-sm rounded-lg border border-blue-200 text-blue-600 bg-blue-500/15  hover:bg-blue-500/20 ">
+            {/*AddIcon图标*/}
+            <div className="flex px-2 py-2.5">
+              <AddIcon />
+            </div>
 
-                      {/*新对话按钮 */}
-                      <div>
-                          <button
-                              className=" py-2 ">
-                              开启新对话
-                          </button>
-                      </div>
-                  </div>
+            {/*新对话按钮 */}
+            <div>
+              <button className=" py-2 ">开启新对话</button>
+            </div>
+          </div>
 
           {/*历史对话*/}
           <ul className="mt-6 space-y-1">
@@ -196,7 +201,7 @@ function ChatPage() {
       >
         {/* 头部导航栏 - 固定在主内容顶部 */}
         {/*进入具体内容页可加上: border-b border-gray-300 */}
-        <header className="sticky top-0 flex flex-row px-5 py-3 justify-between z-10">
+        <header className="sticky top-0 flex flex-row px-5 py-3 justify-between z-10 select-none">
           {/* 左侧按钮 */}
           <div className="flex flex-row text-gray-700">
             <button
@@ -241,22 +246,40 @@ function ChatPage() {
 
         {/* 可滚动的主内容区域 */}
         <main className="flex-grow h-screen justify-center items-center overflow-auto">
-          <div className="flex flex-col text-base mx-5 items-center mt-36 gap-4">
-            <h1 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-indigo-600 via-indigo-500 to-indigo-400 leading-[34px] tracking-[0.38px]">
+          <div className="flex flex-col text-base mx-5 items-center mt-44 gap-5">
+            <h1 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-br from-indigo-600  to-indigo-500 leading-[34px] tracking-[0.38px] select-none">
               有什么可以帮忙的?
             </h1>
             <div
-              className="flex flex-col h-28 w-3/6 rounded-3xl border border-gray-500 bg-gray-50"
+              className="flex flex-col h-auto min-h-28 w-7/12 rounded-3xl border border-gray-500 bg-gray-50"
               id="InputContainer"
             >
-              <div className="mx-3 mt-1">
-                <input
-                  type="text"
-                  className="w-full rounded-lg p-3 pe-12 text-base outline-none bg-inherit"
-                  placeholder="询问任何问题"
-                />
-                <div className="w-full rounded-lg p-3 pe-12 justify-between text-base outline-none bg-inherit">
-                  发送
+              <div className="mx-3 mt-1 flex flex-col bg-inherit mb-2">
+                <textarea
+                  rows={1}
+                  className="w-full rounded-lg p-3 pe-12 pb-6 text-base bg-inherit outline-none resize-none overflow-y-auto"
+                  placeholder="问你所想 畅所欲言"
+                  onInput={(e) => {
+                    e.target.style.height = "auto"; // 先重置高度，防止高度收缩不生效
+                    let currentHeight = e.target.scrollHeight;
+                    let maxHeight =
+                      parseInt(window.getComputedStyle(e.target).lineHeight) *
+                      7;
+                    e.target.style.height = `${
+                      currentHeight < maxHeight ? currentHeight : maxHeight
+                    }px`; // 根据内容调整高度
+                  }}
+                ></textarea>
+                <div className="w-full flex justify-between mt-2">
+                  <button
+                    className="flex flex-row justify-center items-center gap-1 px-2 rounded-full bg-white border-gray-300 border text-black hover:bg-gray-100 transition"
+                  >
+                    <DeepThinkIcon className={"size-4"}/>
+                    <span className="text-sm mb-[2px] select-none">深度思考</span>
+                  </button>
+                  <button className="size-9 bg-indigo-600 text-white rounded-full hover:bg-indigo-500">
+                    <ArrowUpIcon className={"size-9"} />
+                  </button>
                 </div>
               </div>
             </div>
