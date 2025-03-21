880a0101ac4dbcf7b43302df6481de5091e300c6
CXRunfree <2654559534@qq.com>
2025-03-21 19:17:56 +0800

    修改为远程地址

--- backend/config/config.go
+++ backend/config/config.go
@@ -7,27 +7,23 @@ import (
 	"time"
 
 	"github.com/bwmarrin/snowflake"
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 	"gopkg.in/gomail.v2"
 )
 
 var (
 	// MongoDB
-	Mongo_url = "mongodb://ferret:ferret@localhost:27217"
+	Mongo_url string
 
 	//Postgres
-	Host          = "localhost"
-	PG_user       = "postgres"
-	PG_password   = "postgres"
-	PG_dbname     = "user_info"
 	SessionExpire = 7 * 24 * time.Hour // 7天过期
 	PG_dsn        string
 
 	// Redis
 	RedisOpt = redis.Options{
-		Addr:     "localhost:6379", // Redis 服务器地址
-		Password: "",               // Redis 认证密码（如果没有，可留空）
-		DB:       0,                // 使用默认数据库
+		Password: "", // Redis 认证密码（如果没有，可留空）
+		DB:       0,  // 使用默认数据库
 	}
 	// Redis Stream MQ
 	StreamName    = "email_stream"
@@ -58,10 +54,19 @@ var (
 )
 
 func init() {
+	// 加载 .env 文件
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	//Postgres
-	PG_dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", Host, PG_user, PG_password, PG_dbname)
+	PG_dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable",
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	// MongoDB
+	Mongo_url = fmt.Sprintf("mongo://%s:%s@%s", os.Getenv("MONGO_USER"), os.Getenv("MONGO_PASSWORD"), os.Getenv("MONGO_HOST"))
+	// Redis
+	RedisOpt.Addr = os.Getenv("REDIS_ADDR")
 	// Create a new Node with Node number 1
-	var err error
 	SFNode, err = snowflake.NewNode(int64(MachineID))
 	if err != nil {
 		fmt.Println(err)
