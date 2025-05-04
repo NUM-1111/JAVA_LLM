3eb265a26045800592caac3b15bba09989d00728
CXRunfree <2654559534@qq.com>
2025-05-04 12:31:19 +0800

    调整文件

--- frontend/src/components/FileShowPage.jsx
+++ frontend/src/components/FileShowPage.jsx
@@ -1,7 +1,3 @@
-import React from 'react';
-import jsPreviewDocx from "@js-preview/docx";
-import "@js-preview/docx/lib/index.css";
-import jsPreviewExcel from "@js-preview/excel";
-import "@js-preview/excel/lib/index.css";
-import jsPreviewPdf from "@js-preview/pdf";
+function FileShowPage() {}
 
+export default FileShowPage;
